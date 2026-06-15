1ba3cc57722cc75715f29f9b1c51206282614393
Malek Ahmed <malekzenhom38@gmail.com>
2026-06-16 01:49:26 +0200

    update to nextjs 16 and fix config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
